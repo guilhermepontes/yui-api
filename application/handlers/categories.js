5989e4a1b291b539f25881b67e6b2552dbec379a
Guilherme Pontes <guilhermepontes@msn.com>
2018-04-27 22:47:09 +0200

    Add category handler

---
+++ application/handlers/categories.js
@@ -0,0 +1,36 @@
+import Boom from 'boom';
+import Product from './../models/product';
+
+class Categories {
+  static register = [
+    {
+      method: 'GET',
+      path: '/categories',
+      handler: Categories.list,
+    },
+  ]
+
+  static async list() {
+    try {
+      const payload = await Product.find({}).distinct('category');
+      return { payload };
+    } catch(e) {
+      return Boom.badRequest(e);
+    }
+  }
+}
+
+export default Categories;
+
+// export const plugin = {
+//   name: 'categories',
+//   register: async (server, options) => {
+//     server.route({
+//       method: 'GET',
+//       path: '/categories',
+//       handler: async () => {
+
+//       },
+//     });
+//   },
+// };
