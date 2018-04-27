8bca677833a0df7373fa6f279a9c2c8228354d8a
Guilherme Pontes <guilhermepontes@msn.com>
2018-04-27 22:47:37 +0200

    Add component model

---
+++ application/models/component.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const schema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+export default schema;
